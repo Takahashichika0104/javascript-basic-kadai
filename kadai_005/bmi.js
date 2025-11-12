e78e93f92df4412ddb6fd419395e77c34a5c3ca2
Tkahashichika0104 <kk.cc.9225.clover.9480@gmail.com>
2025-11-12 19:13:39 +0900

    .\kadai_005\を追加

---
+++ kadai_005/bmi.js
@@ -0,0 +1,12 @@
+// 体重（kg）と身長（m）を変数に代入
+let weight = 68;  // 例：68kg
+let height = 1.7;  // 例：1.7m
+
+
+// BMI の値を計算
+let bmi = weight/ (height * height);
+
+// 結果をコンソールに出力
+console.log("体重: " + weight + " kg");
+console.log("身長: " + height + " m");
+console.log("BMI: " + bmi);
